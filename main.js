6ec53ba482bbe53faa25f9ee3091d7590ff0d053
Nicole Hemenway <nkhemenway@gmail.com>
2018-01-16 19:26:27 -0800

    add DOMContentLoaded function

---
+++ main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('mainIcons').style.display = 'flex';
+    console.log("DOM fully loaded and parsed");
+  });
