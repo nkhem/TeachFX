f64477b45690fb8fd76c83955e2199fcca58a523
Nicole Hemenway <nkhemenway@gmail.com>
2018-01-16 22:01:56 -0800

    close shop for the night

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    document.getElementById('mainIcons').style.display = 'flex';
-    console.log("DOM fully loaded and parsed");
-  });
+let logoEl = document.getElementById("mainLogo");
+
+document.addEventListener("DOMContentLoaded", e => {
+    // document.getElementById('mainIcons').style.display = 'flex';
+      setTimeout(() => {
+        logoEl.style.display = 'block';
+        
+        //fade logo in
+        if (logoEl.classList.contains('is-paused')){
+          logoEl.classList.remove('is-paused');
+        }
+        slideLogoUp();
+      }, 1000);
+    // }
+  }
+);
+
+let slideLogoUp = () => {
+  // let currMarginTop = parseInt(getComputedStyle(logoEl).marginTop);
+  // console.log("sliding up!" + currMarginTop);
+  // if(currMarginTop > 100){
+  //   logoEl.style.marginTop = currMarginTop - 0.1 + "px";
+  //   slideLogoUp();
+  // }
+};
