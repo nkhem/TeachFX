bc2e6d83ea069962ccca52f5904797b052dd4de0
Nicole Hemenway <nkhemenway@gmail.com>
2018-01-17 10:35:00 -0800

    finish logo animation, start placing main buttons

--- main.js
+++ main.js
@@ -1,36 +1,68 @@
-let logoEl = document.getElementById("main-logo");
-
-
 
 document.addEventListener("DOMContentLoaded", e => {
-    var blankiPhoneImg = new Image();
-    
+    let blankiPhoneImg = new Image();
     blankiPhoneImg.src = "images/blankiPhone.png";
-    blankiPhoneImg.onload = () => {
-      console.log("image loaded");
-        // do whatever here, add it to the background, append the image ect.
-        document.getElementsByClassName("iphone-img")[0].appendChild(blankiPhoneImg);   
-    };
     
-    document.getElementById('main-icons').style.display = 'flex';
-      setTimeout(() => {
-        logoEl.style.display = 'block';
+    //wait for blank iphone image to load before animations begin
+    blankiPhoneImg.onload = () => {
+        document.getElementsByClassName("iphone-img")[0].appendChild(blankiPhoneImg);   
+        animateLogo();
         
-        //fade logo in
-        if (logoEl.classList.contains('is-paused')){
-          logoEl.classList.remove('is-paused');
-        }
-        slideLogoUp();
-      }, 1000);
-    // }
+        // let i = document.getElementsByClassName('main-icons')[0].childElementCount.
+        // setInterval(()=>{
+        //   document.getElementsByClassName('main-icons')[i].style.display = 'flex';  
+        // }, 700);
+    };
   }
 );
 
-let slideLogoUp = () => {
-  // let currMarginTop = parseInt(getComputedStyle(logoEl).marginTop);
-  // console.log("sliding up!" + currMarginTop);
-  // if(currMarginTop > 100){
-  //   logoEl.style.marginTop = currMarginTop - 0.1 + "px";
-  //   slideLogoUp();
-  // }
+let animateLogo = () => {
+  let logoEl = document.getElementById("main-logo");
+  setTimeout(() => {
+    fadeElementIn(logoEl);
+  }, 1000);
+  
+  //wait until fadeElementIn is done plus 1 sec, then slide logoup
+  logoEl.addEventListener("webkitAnimationEnd", () => {
+    setTimeout(() => {
+      slideElement(logoEl, 0, -150);
+    }, 1500);
+  } ,false);
+};
+let fadeElementIn = logoEl => {
+  logoEl.style.display = 'block';
+  
+  //fade logo in
+  if (logoEl.classList.contains('is-paused')){
+    logoEl.classList.remove('is-paused');
+  }
+};
+let slideElement = (slidingEl, xScrollDistance, yScrollDistance) => {
+  setInterval(()=> {
+    let shouldScrollHorizontally = Math.abs(xScrollDistance) > 1;
+    let shouldScrollVertically = Math.abs(yScrollDistance) > 1;
+    
+    //end function if we've hit our destination
+    if(!(shouldScrollHorizontally || shouldScrollVertically)){
+      clearInterval(slideElement);
+    }
+    
+    //figure out where slidingEl is horizontally
+    let currLeft = slidingEl.style.left ? parseInt(slidingEl.style.left) : 0;
+    //shift slidingEl 1 px horizontally
+    if(shouldScrollHorizontally){
+      let diff = xScrollDistance < 1 ? 1 : -1;
+      slidingEl.style.left = currLeft - diff + "px";
+      xScrollDistance += diff;
+    }
+
+    //figure out where slidingEl is vertically
+    let currTop = slidingEl.style.top ? parseInt(slidingEl.style.top) : 0;
+    //shift slidingEl 1 px vertically
+    if(shouldScrollVertically){
+      let diff = yScrollDistance < 1 ? 1 : -1;
+      slidingEl.style.top = currTop - diff + "px";
+      yScrollDistance += diff;
+    }
+  }, 1);
 };
