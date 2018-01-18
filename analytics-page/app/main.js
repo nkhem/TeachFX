d0475a2c804c5d2915c28fbcd2654203c32cdc74
Nicole Hemenway <nkhemenway@gmail.com>
2018-01-18 15:51:29 -0800

    import line-chart from https://github.com/adeveloperdiary/react-d3-charts/blob/gh-pages/01_Visitor_Dashboard/lib/charts/LineChart.jsx

--- analytics-page/app/main.js
+++ analytics-page/app/main.js
@@ -2,10 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-
-// let start = () => {
-//   // d3.select
-//   console.log('starting!');
-// };
-
 ReactDOM.render(<App />, document.getElementById('app'));
