2f0ebb7eb35c3a589223582689681624c600a8d7
Nicole Hemenway <nkhemenway@gmail.com>
2018-01-19 10:30:19 -0800

    add data security webinar notes

---
+++ analytics-page/react/app/components/analytics-page.js
@@ -0,0 +1,15 @@
+import React, { PropTypes } from 'react';
+import BarChart from './bar-chart';
+
+class AnalyticsPage extends React.Component {
+  render () {
+    return (
+      <div>
+        <BarChart data={[5,10,1,3]} size={[500,500]} />
+        <p>{ this.props.dummyData[0].classTitle }</p>
+      </div>
+    );
+  }
+}
+
+export default AnalyticsPage;
