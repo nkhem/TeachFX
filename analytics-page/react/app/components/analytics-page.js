64a6e5e6eb53a331b8333080b1ea52525b39948d
Nicole Hemenway <nkhemenway@gmail.com>
2018-01-19 16:59:29 -0800

    get a line chart up!

--- analytics-page/react/app/components/analytics-page.js
+++ analytics-page/react/app/components/analytics-page.js
@@ -1,13 +1,35 @@
 import React, { PropTypes } from 'react';
-import BarChart from './bar-chart';
+import SimpleLineChart from './simple-line-chart';
+
+const dummyData = [
+  {
+    classTitle: 'English',
+    classDate: new Date("2015-03-25"),
+    studentTalk: 51,
+    teacherTalk:44,
+    otherTalk: 5,
+  },
+  {
+    classTitle: 'Math',
+    classDate: new Date("2015-03-26"),
+    studentTalk: 60,
+    teacherTalk: 35,
+    otherTalk: 5,
+  },
+  {
+    classTitle: 'History',
+    classDate: new Date("2015-03-27"),
+    studentTalk: 70,
+    teacherTalk: 20,
+    otherTalk: 10,
+  },
+];
 
 class AnalyticsPage extends React.Component {
   render () {
     return (
       <div>
-        <BarChart data={[5,10,1,3]} size={[500,500]} />
-        <p>{ this.props.dummyData[0].classTitle }</p>
-        <div id="container" />
+        <SimpleLineChart data={ dummyData }/>
       </div>
     );
   }
